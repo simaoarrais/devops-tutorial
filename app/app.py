3852f8ba51e2453670a655c3aaad1c4d4723a979
Simão Arrais <simao.arrais@wit-software.com>
2026-04-27 13:34:26 +0100

    fix for workflow #2

--- app/app.py
+++ app/app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return jsonify({
-    "message": "Hello from the DevOps tutorial! - V3!",
-    "version": os.environ.get("APP_VERSION", "dev"),
-  })
+    return jsonify({
+      "message": "Hello from the DevOps tutorial! - V3!",
+      "version": os.environ.get("APP_VERSION", "dev"),
+    })
 
 @app.route("/health")
 def health():
-  return jsonify({"status": "ok"}), 200
+    return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
